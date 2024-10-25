514e4d81bcf18c701d146b4b94a646b05e94aaa4
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2024-10-25 03:22:40 +0000

    .

---
+++ Lesson 14(Python as a calculator).py
@@ -0,0 +1,3 @@
+#Now we are going to complete new side of the print() function
+#infact it can do something more
+print(2+2)
---
+++ Lesson 15(Basic Operators).py
@@ -0,0 +1,21 @@
+#An Operator is a symbol of the programming language which is able to operate on the values
+#For example + ass two numbers
+#Recognizable arithmetic Operations
+# +,-,*,/,//,%,**
+# Addition(+)
+print(2+3)
+#Subtraction(-)
+print(8-5)
+#Multiplication(*)
+print(5*8)
+#Division(/)
+print(40/4)
+#Exponentation(**)
+print(2**5)
+#interger division(Floor division)(//)
+print(6//3)
+#predict the result
+print(-6//4)
+print(6//-4)
+#Remainder modulo(%)
+print(14%4)
