bfd44bee7c14c93997a39ee7f06dd60440c06d2b
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2024-10-25 02:34:09 +0000

    Lesson 10,11,12

--- Lesson 10(Integer).py
+++ Lesson 10(Integer).py
@@ -1,1 +1,20 @@
-#we will say that number handling by modern 
+#we will say that number handling by modern computers are two types
+# 1. Integers that is ,those which are devoid of the fractional part;
+# 2.Floating point numbers (Or simply floats) that contain the fraction part.
+
+#This definition is not quite accurate but quite sufficient for now
+
+#Take for example eleven million one hundred one hundred.if you look pencil in your hand right now ,you would write number like this "11,111,111" or like this 11.111.111 or even like this 11 111 111
+
+#It is clear this provision makes it easier to read ,especially when the number consist of many digit
+#However Python doesn't accept this what python does allow though is underscores(_) or there can write it number like this (11111111) or this(11_111_111)
+
+#Octal and hexadecimal numbers
+#it an integer is preceded by an 00 or Oo(zero-o), it will be treated as an Octal value.This means that the number must contain digit taken the [0..7] range only
+print(0o123)#)0o123 is an Octal number with a decimal value 83
+
+#Hexadecimal
+#The second conversion allow us to use the hexadecimal numbers,such numbers should be preceded by the prefix 0x (zero-x)
+#Try this
+print(0x123)
+
---
+++ Lesson 11(float).py
@@ -0,0 +1,33 @@
+#it is time to talk about another type,which is designed and to store the number that a mathematician will say non-empty decimal fraction
+#whenever we use the term two and half or minus zero point four we think of numbers which a float.consider the floating point 2.5 and -0,4
+
+print(type(2.5))
+print(type(-0.4))
+
+#int vrs float
+#Decimal point are essential for recognizing numbers in python look at these;
+print(type(4))#is an integer
+print((type(4.0)))#is a float
+
+
+#Also it's not only point that make  a float . You can use Letter e.
+ #When you want to use any number that are very large or small,you can use scientific notation
+ #for example the speed of light,expressed in meter per second.written directly like this
+ #300000000
+ #To avoid writing out many zeros,physics textbook use an abbreviation form,which you have probably already seen:
+ # 3 x 10^8
+
+ #it reads 3 times  10 to the power 8;
+
+ #in python the same effect is achieved in a slightly different way - take a look
+#3E8
+
+#the letter E(you can use lower-case (e))-it cones from the word exponent)
+
+#Note
+#The exponent(the value of E) has to be an integer
+#the base(the value in front of the E) can either be an integer or float
+
+print(0.000000000007)
+
+
