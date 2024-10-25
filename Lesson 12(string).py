329eb73df37fe01ec0b9399a5e340d22cc73000c
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2024-10-25 03:03:34 +0000

    Keep learning

---
+++ Lesson 12(string).py
@@ -0,0 +1,15 @@
+#string are used for all kinds of  not numbers
+#You already know a bit about them .String need quote the way float in need point
+
+#This is a typical string:eg;'I am a string'
+
+#The first is based on the concept we already know escape character,which you should remember played by the backlash.
+
+print("I like\'Monty Python\'")
+
+#Or
+
+print('I like "Monty Python" ')
+
+#So now print (I'm Monty Python)
+#Good luck
---
+++ Lesson 13(Boolean).py
@@ -0,0 +1,3 @@
+#This are not Obvious as the previous ones,as they're used to represent abstract value
+print(type(True))
+print(type(False))
