649fde6ad1bd87692f5ba87f0dd0d151dd939164
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2024-11-01 03:00:19 +0000

    .

---
+++ Lesson 21(input function).py
@@ -0,0 +1,5 @@
+#The input() function is able to read data entered by the user and return the same data running program
+#The program can manipulate the data making the code truly interactive
+print("Tell me anything:")
+anything=input()
+print("Hmm....",anything,"...really")
---
+++ Lesson 22(The input() function with an argument).py
@@ -0,0 +1,3 @@
+#The input() function can do something else:it can prompt the user without any help from print
+anything=input("Tell me anything:")
+print("Hmm...",anything,"...Really")
