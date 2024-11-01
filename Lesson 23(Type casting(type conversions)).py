5ff39d87e1b313f57ab583b405425076ee76e08f
Anim Michael Asante <160376570+Obofour@users.noreply.github.com>
2024-11-01 03:35:40 +0000

    .

---
+++ Lesson 23(Type casting(type conversions)).py
@@ -0,0 +1,6 @@
+#Python offer two simple function to specify a type and solve this problem-here they are:int() and float()
+#int() takes one argument
+#float() function takes one argument
+anything=int(input("Enter a number:"))
+something=anything**2
+print(anything,"to the power of 2 is ",something)
---
+++ Lesson 24(String operators).py
@@ -0,0 +1,10 @@
+#it simply concatenates(glue) two string into one
+#Don't forget -if you want the (+) sign to be a concatenator not an adder,you must ensure that both its argument are string
+fname=str(input('May I know your first name, Please: '))
+lastnam=str(input('May I have your Last name,please: '))
+print("Thank you")
+print("\n Your name is "+fname+" "+lastnam+".")
+
+#Replication
+#the asterisk sign when applied to a string and number becomes a replication operator
+print("Student"*3)
